a99ae57b893e55305ad8ff3d3601f8b02147a067
Nurlykhan02 <ntaubuzar@bk.ru>
2024-12-26 15:21:42 +0500

    cluster fix with min count

--- clusterManager.js
+++ clusterManager.js
@@ -1,18 +1,26 @@
 import cluster from 'cluster';
 import os from 'os';
 
-export function setupCluster(workerCallback) {
+export function setupCluster(workerCallback, minWorkers = 1) {
   if (cluster.isPrimary) {
     const numCPUs = os.cpus().length;
     console.log(`Primary process ${process.pid} is running`);
 
-    for (let i = 0; i < numCPUs; i++) {
+    const totalWorkers = Math.max(minWorkers, numCPUs);
+
+    for (let i = 0; i < totalWorkers; i++) {
       cluster.fork();
     }
 
     cluster.on('exit', (worker, code, signal) => {
-      console.log(`Worker ${worker.process.pid} exited. Restarting...`);
-      cluster.fork();
+      console.log(`Worker ${worker.process.pid} exited. Because: ${code, signal} Restarting...`);
+
+      const activeWorkers = Object.keys(cluster.workers).length;
+
+      if (activeWorkers < minWorkers) {
+        console.log(`Active workers below minimum (${activeWorkers}/${minWorkers}). Starting a new worker.`);
+        cluster.fork();
+      }
     });
   } else {
     workerCallback();
