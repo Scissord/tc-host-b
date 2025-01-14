b6a1a28e2f15b31ca347a8c4152e2e7f0d9b2ce2
nurdaulet <scissxrd@gmail.com>
2025-01-14 18:50:19 +0500

    fix

--- utils/hideString.js
+++ utils/hideString.js
@@ -1,11 +1,21 @@
+// export default function hideString(string) {
+//   const str = String(string);
+
+//   const maskLength = Math.ceil(str.length * 0.4);
+
+//   const visiblePart = str.slice(0, str.length - maskLength);
+
+//   const maskedPart = '*'.repeat(maskLength);
+
+//   return visiblePart + maskedPart;
+// };
+
 export default function hideString(string) {
   const str = String(string);
 
-  const maskLength = Math.ceil(str.length * 0.4);
-
-  const visiblePart = str.slice(0, str.length - maskLength);
+  const maskLength = str.length;
 
   const maskedPart = '*'.repeat(maskLength);
 
-  return visiblePart + maskedPart;
+  return maskedPart;
 };
