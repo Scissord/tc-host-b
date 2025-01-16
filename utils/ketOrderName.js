cffe6ad73e8ad7bf8594a7de75e12621d9f0f419
Nurlykhan02 <ntaubuzar@bk.ru>
2025-01-16 18:22:27 +0500

    fix

--- utils/ketOrderName.js
+++ utils/ketOrderName.js
@@ -1,18 +1,20 @@
 export function getOrderName (goodID, quantity) {
+
+    console.log('')
     const goodsCodes = {
-        198508: "Prostrong",
-        196548: "Be_clean",
+        200500: "Prostrong",
+        200503: "Be_clean",
         192114: "Articuline",
-        186491: "Cardio_Sei",
-        199030: "Femiston",
-        192001: "Eropower",
-        202690: "ManBalance",
+        200505: "Cardio_Sei",
+        200502: "Femiston",
+        200501: "Eropower",
+        212257: "ManBalance",
         193976: "Cardio_Sei_VARIKOZ",
         202914: "Flex_balance",
         204116: "Body_balance",
-        204120: "LibidoFortis",
-        204118: "AlcoBalance",
-        204117: "EroKing"
+        212251: "LibidoFortis",
+        212253: "AlcoBalance",
+        212254: "EroKing"
     };
 
     if (goodsCodes[goodID]) {
