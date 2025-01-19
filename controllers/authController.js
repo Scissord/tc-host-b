a6468805e5cfd4a7316cdf3c1ed9006641d0a73c
nurdaulet <scissxrd@gmail.com>
2025-01-19 11:17:23 +0500

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,80 +1,18 @@
 import jwt from 'jsonwebtoken';
+import requestIp from 'request-ip';
 import * as User from "#models/user.js";
 import * as UserToken from "#models/user_token.js";
 import { validateAuth } from "#services/auth/validate.js";
 import { getUserInfo, getUserAssignedRole, getUserAbilitiesId } from "#services/ability/user.js";
 import generateTokens from "#utils/generateTokens.js";
 import ERRORS from "#constants/errors.js";
-// import sha256 from 'js-sha256';
-
-// export const signup = async (req, res) => {
-// 	try {
-// 		let { login, password, confirm, gender } = req.body;
-
-//     login = login.trim();
-//     password = password.trim();
-//     confirm = confirm.trim();
-
-// 		const user = await User.findWhere({ login });
-
-// 		if (password !== confirm) {
-// 			return res.status(400).send({ error: "Passwords don't match" });
-// 		}
-// 		if (user) {
-// 			return res.status(400).send({ error: "Username or phone already exists" });
-// 		}
-// 		if (password.length < 6) {
-// 			return res.status(400).send({ error: "Password must be at least 6 characters long" });
-// 		}
-
-// 		const salt = await bcrypt.genSalt(10);
-// 		const hashedPassword = await bcrypt.hash(password, salt);
-
-// 		// https://gravatar.com/
-//     const address = String(login).trim().toLowerCase();
-//     const hash = sha256(address);
-//     const avatar = `https://www.gravatar.com/avatar/${hash}?d=wavatar`;
-
-//     const newUser = await User.create({
-//       login,
-//       password: hashedPassword,
-//       gender,
-// 			avatar
-//     })
-
-// 		if(newUser) {
-// 			const { accessToken, refreshToken } = generateTokens(newUser.id);
-
-//       await UserToken.create({
-//         user_id: newUser.id,
-//         refresh_token: refreshToken,
-//         expires_at: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
-//       });
-
-//       res.cookie("refreshToken", refreshToken, {
-//         maxAge: 30 * 24 * 60 * 60 * 1000,
-//         httpOnly: true, // XSS
-//         sameSite: "strict", // CSRF
-//         secure: process.env.NODE_ENV === "production"
-//       });
-
-// 			return res.status(201).send({
-//         message: "Successfully created",
-//         user: newUser,
-//         accessToken
-//       });
-// 		} else {
-// 			return res.status(400).send({ error: "Invalid user data" });
-// 		}
-// 	}	catch (err) {
-// 		console.log("Error in post signup controller", err.message);
-// 		res.status(500).send({ error: "Internal Server Error" });
-// 	};
-// };
 
 export const signin = async (req, res) => {
   try {
     let { login, password, entity } = req.body;
+
+    const ip = requestIp.getClientIp(req);
+    console.log(ip);
 
     // 1. validation
     const result = await validateAuth(login, password, entity);
