4afaea7fb2fe78b9b58c2965431ae2b063312cef
Nurlykhan02 <ntaubuzar@bk.ru>
2025-01-20 13:02:03 +0500

    fix

--- migrations/20250120075126_alter_table_scheduled_tasks.js
+++ migrations/20250120075126_alter_table_scheduled_tasks.js
@@ -5,7 +5,7 @@
 export const up = async function (knex) {
     return knex.schema.alterTable('scheduled_tasks', (table) => {
         table.string('scheduled_time').alter().nullable()
-        table.timestamp('scheduled_time_timestamp').nullable()
+        table.time('scheduled_time_timestamp').nullable()
     });
 };
 
