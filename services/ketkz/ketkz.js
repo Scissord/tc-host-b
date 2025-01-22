671a103e829da98f6751fe00c9f3c0a77ba3113a
Nurlykhan02 <ntaubuzar@bk.ru>
2025-01-22 13:34:58 +0500

    fix

--- services/ketkz/ketkz.js
+++ services/ketkz/ketkz.js
@@ -4,6 +4,7 @@ import * as OrderGood from '#models/order_item.js'
 import * as City from '#models/city.js'
 import * as KetUtils from '#utils/ketOrderName.js'
 import { getCityCode } from '#utils/cityCode.js';
+import { getKetStatus } from '#utils/ketStatusArray.js'
 
 dotenv.config();
 
@@ -227,6 +228,13 @@ export const checkSendedOrders = async () => {
 			return [];
 		}
 
+		const orderStatuses = {};
+		orders.forEach(order => {
+			orderStatuses[order.id] = +order.sub_status_id;
+		});
+
+		console.log("Статусы заказов:", JSON.stringify(orderStatuses, null, 2));
+
 		const ext_ids = orders.map(order => order.id);
 		console.log("Отправляемые ext_id:", ext_ids);
 
@@ -246,11 +254,51 @@ export const checkSendedOrders = async () => {
 
 			if (response.ok) {
 				const data = await response.json();
-				console.log("Ответ API:", data);
-				return data;
+				console.log("Ответ API:", JSON.stringify(data, null, 2));
+
+				Object.values(data).forEach(apiResponse => {
+					const id = apiResponse.ext_id;
+					console.log(`Обработка заказа ID: ${id}`);
+
+					if ([3, 47, 27].includes(orderStatuses[id])) { // kd
+						console.log(`Обработка KD заказа ID: ${id}`);
+						const ss_id = getKetStatus(apiResponse.status_cur);
+
+						if (ss_id) {
+							console.log(`Обновление KD заказа ID: ${id}, новый статус: ${ss_id}`);
+							Order.update(id, {
+								sub_status_id: ss_id,
+								total_sum: apiResponse.total_price
+							});
+						} else {
+							console.log(`Статус ${apiResponse.status_cur} не найден для KD заказа ID: ${id}`);
+						}
+
+					} else if ([13, 48, 26].includes(orderStatuses[id])) { // pd
+						console.log(`Обработка PD заказа ID: ${id}`);
+						if (apiResponse.send_status === 5) {
+							console.log(`Обновление PD заказа ID: ${id}, новый статус: 6`);
+							Order.update(id, {
+								sub_status_id: 6,
+								total_sum: apiResponse.total_price
+							});
+						} else if (apiResponse.send_status === 4) {
+							console.log(`Обновление PD заказа ID: ${id}, новый статус: 46`);
+							Order.update(id, {
+								sub_status_id: 46,
+								total_sum: apiResponse.total_price
+							});
+						} else {
+							console.log(`Неизвестный статус отправки ${apiResponse.send_status} для PD заказа ID: ${id}, сумма: ${apiResponse.total_price}`);
+						}
+
+					} else {
+						console.log(`ID заказа: ${id}, Статус ${orderStatuses[id]} не найден`);
+					}
+				});
+
 			} else {
 				console.error(`Ошибка запроса: ${response.status} ${response.statusText}`);
-				return [];
 			}
 		} catch (fetchError) {
 			console.error("Ошибка при выполнении запроса:", fetchError.message);
@@ -261,6 +309,8 @@ export const checkSendedOrders = async () => {
 		throw new Error("Не удалось обработать заказы");
 	}
 };
+
+
 
 
   
