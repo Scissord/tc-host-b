8e48fc9884cdb352483a394a8ac79bfde9af744a
Nurlykhan02 <ntaubuzar@bk.ru>
2025-01-23 20:20:20 +0500

    fix

--- models/order.js
+++ models/order.js
@@ -843,7 +843,8 @@ export const getOrderStatisticForOperator = async (start, end, operator_id = nul
           SUM(
             CASE
               WHEN 
-              (o.approved_at IS NOT NULL)
+              (o.approved_at IS NOT NULL 
+                  AND (o.cancelled_at IS NULL OR o.approved_at > o.cancelled_at))
               OR (o.returned_at IS NOT NULL)
               OR (o.shipped_at IS NOT NULL)
               OR (o.buyout_at IS NOT NULL)
