ef42360cec95492e62b115a92573577e8495fa69
Nurlykhan02 <ntaubuzar@bk.ru>
2025-01-23 19:56:02 +0500

    fix

--- models/order.js
+++ models/order.js
@@ -843,7 +843,7 @@ export const getOrderStatisticForOperator = async (start, end, operator_id = nul
           SUM(
             CASE
               WHEN o.approved_at IS NOT NULL 
-                  AND (o.cancelled_at IS NULL OR o.approved_at > o.cancelled_at)
+                  AND (o.returned_at IS NULL OR o.returned_at > o.cancelled_at)
               THEN 1 ELSE 0
             END
           ) AS accepted_orders,
