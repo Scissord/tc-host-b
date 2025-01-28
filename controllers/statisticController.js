1254d17d4645dc41518b32cf048f21686104b872
Nurlykhan02 <ntaubuzar@bk.ru>
2025-01-29 03:09:01 +0500

    fix

--- controllers/statisticController.js
+++ controllers/statisticController.js
@@ -249,7 +249,7 @@ export const getOperatorStatistic = async (req, res) => {
 export const fromHundredThousand = async (req, res) => {
   try {
     console.log("Начинаем обработку заказов с ID 100000...");
-    const orders = await Order.getFrom(80001, 90000);
+    const orders = await Order.getFrom(90001, 100000);
 
     if (!orders || orders.length === 0) {
       console.log("Заказы не найдены.");
