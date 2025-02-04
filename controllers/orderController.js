7d9f22802fbd02162fceabf16cd182c19b4662d9
nurdaulet <scissxrd@gmail.com>
2025-02-04 17:42:24 +0500

    fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -706,7 +706,7 @@ export const unloading = async (req, res) => {
 
 export const sync = async (req, res) => {
   // Начинаем с 1 до 10000
-  for(let i = 12; i < 10000; i++) {
+  for(let i = 12; i <= 50; i++) {
     // Ищем заказ
     let orderResponse = null;
     try {
